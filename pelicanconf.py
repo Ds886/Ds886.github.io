2057d3e2245d2d2405da41c8c09ba914159607b7
Ds886 <2217864+Ds886@users.noreply.github.com>
2023-01-25 01:32:37 +0200

    remove social and links

--- pelicanconf.py
+++ pelicanconf.py
@@ -18,14 +18,10 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('To be added', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = ()
 
 DEFAULT_PAGINATION = 15
 
